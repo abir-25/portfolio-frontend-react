03faae9ce6ddacaddf7d2875ad0b48cc3ba7b668
abir-25 <abirbgc25@gmail.com>
2023-06-03 01:22:54 +0600

    imgae added in description page

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navbar } from "../components";
 import { About, Footer, Header, Skills, Testimonial, Work } from "../container";
 
 const Home = () => {
+  useEffect(() => {
+    fetch("https://get.geojs.io/v1/ip/geo.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        localStorage.setItem("geo-location", JSON.stringify(data));
+      });
+  }, []);
+
   return (
     <div>
       <Navbar />
--- src/container/WorkDescription/WorkDescription.jsx
+++ src/container/WorkDescription/WorkDescription.jsx
@@ -4,9 +4,12 @@ import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import { useParams } from "react-router-dom";
 import "./WorkDescription.scss";
+import { Navbar } from "../../components";
 
 const WorkDescription = () => {
   const [work, setWork] = useState([]);
+  const [location, setLocation] = useState();
+  const [imageSrc, setImageSrc] = useState();
   const { id } = useParams();
 
   useEffect(() => {
@@ -14,11 +17,58 @@ const WorkDescription = () => {
 
     client.fetch(query).then((data) => {
       setWork(data);
+      setImageSrc(urlFor(data[0]?.imgUrl && data[0]?.imgUrl).url());
     });
   }, []);
+  // console.log(imageSrc);
 
-  console.log(work);
-  return <div>{id}</div>;
+  useEffect(() => {
+    setLocation(JSON.parse(localStorage.getItem("geo-location")));
+  }, []);
+
+  // console.log(work);
+  return (
+    <div>
+      <Navbar />
+      <div className="app__container app__whitebg">
+        <div className="app__work-description-container">
+          <h2 className="work-title-text">{work[0]?.longTitle}</h2>
+          <div className="tags-container">
+            {work[0]?.tags?.map((tag) => tag !== "All" && <span>{tag}</span>)}
+          </div>
+          <div className="work-summary-container">
+            <div className="recent-work-summary-item">
+              <p>Completion Time</p>
+              <h4>6 Weeks</h4>
+            </div>
+            <div className="recent-work-summary-item">
+              <p>Fee charged</p>
+              <h4>
+                {location?.country_code === "BD"
+                  ? `৳${work[0]?.feeBD}`
+                  : `$${work[0]?.feeNonBD}`}
+              </h4>
+            </div>
+          </div>
+
+          <div className="work-image-container">
+            <div className="image-container">
+              <div className="portfolio_preview_image ">
+                <div className="view_full_page">
+                  <p>Click To View Full Preview</p>
+                </div>
+                <img
+                  src={imageSrc}
+                  alt={work[0]?.title}
+                  className="portfolio-item-image"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WorkDescription;
